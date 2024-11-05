ca74493b50ce8f450208cc12242811bf1dd88a21
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2024-11-04 22:26:26 -0600

    anadir los archivos al repo

---
+++ app/src/main/java/com/diegoalfaro/myapplication/User.kt
@@ -0,0 +1,15 @@
+package com.diegoalfaro.myapplication
+
+data class User(var name: String, var age: Int) {
+    fun isAdult(): Boolean {
+        return age > 18
+    }
+
+    fun isTeenager(): Boolean {
+        return age in 15..18
+    }
+
+    fun isChild(): Boolean {
+        return age < 13
+    }
+}
---
+++ app/src/test/java/com/diegoalfaro/myapplication/UserTest.kt
@@ -0,0 +1,26 @@
+package com.diegoalfaro.myapplication
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class UserTest {
+
+    @Test
+    fun testisAdult() {
+        val user = User("Nico", 19)
+        assertTrue(user.isAdult())
+    }
+
+    @Test
+    fun testisTeen() {
+        val user = User("Nico", 16)
+        assertTrue(user.isTeenager())
+    }
+
+    @Test
+    fun testisChild() {
+        val user = User("Nico", 12)
+        assertTrue(user.isChild())
+    }
+
+}
